abad5bc1b004465c4f9edd8cb33879fb8af8bffd
Soumadip <39thdecember@gmail.com>
2026-05-29 22:10:18 +0530

    feat: auth & usersApiSlice ✅

---
+++ frontend/src/slices/authSlice.js
@@ -0,0 +1,25 @@
+// Slice to set the user-credentials to the localStorage and remove them.
+// This slice doesn't deal with APIs.
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  userInfo: localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo"))
+    : null,
+};
+
+// Now let's create our slice
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setCredentials: (state, action) => {
+      state.userInfo = action.payload;
+      localStorage.setItem("userInfo", JSON.stringify(action.payload));
+    },
+  },
+});
+
+export const { setCredentials } = authSlice.actions;
+export default authSlice.reducer;
---
+++ frontend/src/slices/usersApiSlice.js
@@ -0,0 +1,20 @@
+// Similar to producstApiSlice.
+
+import { USERS_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/auth`,
+        method: "POST",
+        body: data,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+  }),
+});
+
+// export em
+export const { useLoginMutation } = usersApiSlice;
